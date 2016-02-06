b1996748855dfd570b47bf7fbca8e1bdd1077b66
Dmitry Fomin <difomin2@gmail.com>
2016-02-07 00:14:30 +0300

    update

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?
-	$hostname = "localhost";
-	$username = "difomin";
-	$password = "12345";
-	$dbName = "mfin";
-	$link = mysql_connect($hostname,$username,$password) OR DIE("Don't connect with database!"); 
-	mysql_query ("set character_set_client='utf8'");
-	mysql_query ("set character_set_results='utf8'");
-	mysql_query ("set collation_connection='utf8_general_ci'");
-	mysql_select_db($dbName, $link) or die(mysql_error());  
+$hostname = "localhost";
+$username = "difomin";
+$password = "12345";
+$dbName = "mfin";
+$link = mysqli_connect($hostname,$username,$password) OR DIE("Don't connect with database!");
+mysqli_query ($link,"set character_set_client='utf8'");
+mysqli_query ($link,"set character_set_results='utf8'");
+mysqli_query ($link,"set collation_connection='utf8_general_ci'");
+mysqli_select_db($link, $dbName) or die(mysqli_error($link));
 ?>
